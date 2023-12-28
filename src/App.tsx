e8bae838275bd7839dd4c4d22b1d42edbd7a765f
Rahul Raj <raj.rahul686@gmail.com>
2023-12-29 04:28:53 +0530

    provide routing to Product Page in App components

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./styles/App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
-
+import Products from "./components/Products/Products";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<Products />} />
       </Routes>
     </BrowserRouter>
   );
