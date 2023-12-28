7db8bf5c877d9ceb8a842d1c2b3025a8d3ac5eff
Rahul Raj <raj.rahul686@gmail.com>
2023-12-29 03:43:07 +0530

    Implement Latest Trend card with product image and it's name

---
+++ src/components/LatestTrend/LatestTrend.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import "./LatestTrend.scss";
+import { latestTrendApi } from "../../db/data";
+import { latestTrendProducts } from "../../db/types/products";
+import { useNavigate } from "react-router-dom";
+
+const LatestTrend: React.FC = () => {
+  const [latestTrendsData, setLatestTrendsData] =
+    useState<latestTrendProducts[]>();
+  const navigate = useNavigate();
+  const getLatestTrendslist = async () => {
+    try {
+      const data = await latestTrendApi.getlatestTrend();
+      setLatestTrendsData(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  useEffect(() => {
+    getLatestTrendslist();
+  }, []);
+  const navigateToProductsPage = () => {
+    navigate("/products");
+  };
+  return (
+    <div className="latestTrendWrapper">
+      <h2>Latest Trends</h2>
+      <div className="latest_trend_products">
+        {latestTrendsData?.map((trendProduct, i) => {
+          console.log(trendProduct);
+          return (
+            <div
+              key={i}
+              className="latest_trend_product"
+              onClick={navigateToProductsPage}
+            >
+              <img src={trendProduct.latestProductImage} alt="img" />
+              <div>{trendProduct.latestProductName}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default LatestTrend;
